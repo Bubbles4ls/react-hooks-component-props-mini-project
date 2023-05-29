8ac16eb0752c835fd3cba81d1694ee7c8ecaf114
Surayah Holt <+>
2023-05-28 23:38:57 -0400

    lab done

---
+++ src/components/Article.js
@@ -0,0 +1,17 @@
+import React from "react";
+//import ReadTime from "src/components/ReadTime.js";//
+
+function Article({ title, date="January 1, 1970", preview, minutes }) {
+
+
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
+        </article>
+
+    )
+}
+
+export default Article
